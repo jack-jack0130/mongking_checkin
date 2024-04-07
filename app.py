bb128702f8696ab86976f59c3e5470993992cf16
Jack Cheng <jack-jack0130>
2024-04-07 18:19:17 +0800

    'mongking_checkin'

--- app.py
+++ app.py
@@ -150,8 +150,8 @@ def checkin17(floor, room):
 def checkin17_02(room):
     if room == 2:
         return render_template('17inside02.html', room=room)
-    if room == 8:
-        return render_template('17inside08.html', room=room)
+    elif room == 8:
+        return render_template('17inside02.html', room=room)
     else:
         return render_template('17inside.html', room=room)
 
